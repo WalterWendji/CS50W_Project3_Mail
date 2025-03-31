ee00127a47a6758c2118e25e04cce62da3be48dc
WalterWendji <rahaman.dongmepi@stud.th-owl.de>
2025-03-31 15:07:54 +0200

    the 2 last tasks h of issue #2 have been successfully implemented

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Using JavaScript, HTML, and CSS, complete the implementation of your single-page
 	- [x] You’ll likely want to make a `GET` request to `/emails/<mailbox>` to request the emails for a particular mailbox.
     - [x] When a mailbox is visited, the application should first query the API for the latest emails in that mailbox.
     - [x] When a mailbox is visited, the name of the mailbox should appear at the top of the page (this part is done for you).
-    - [ ] Each email should then be rendered in its own box (e.g. as a `<div>` with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
-    - [ ] If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
+    - [x] Each email should then be rendered in its own box (e.g. as a `<div>` with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
+    - [x] If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
 - [ ] **View Email**: When a user clicks on an email, the user should be taken to a view where they see the content of that email.
 	- [ ] You’ll likely want to make a `GET` request to `/emails/<email_id>` to request the email.
     - [ ] Your application should show the email’s sender, recipients, subject, timestamp, and body.
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -24,16 +24,30 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
   if (mailbox === 'inbox') {
 
-    fetch('/emails/inbox') 
+    load_inbox();
+
+  } else if (mailbox === 'sent') {
+
+    load_sentbox(mailbox);
+
+  } else if (mailbox === 'archive') {
+    load_archivebox();
+  }
+
+}
+
+function load_inbox() {
+  fetch('/emails/inbox')
     .then(response => response.json())
     .then(mails_from_inbox => {
       if (mails_from_inbox.length === 0) {
@@ -43,22 +57,14 @@ function load_mailbox(mailbox) {
         document.querySelector("#emails-view").append(message_element)
 
       } else {
-
-        const ul_element = document.createElement('ul')
-        mails_from_inbox.forEach((mail) => {
-          const li_element = document.createElement('li')
-          li_element.innerHTML = `${mail.sender} ${mail.subject} ${mail.timestamp}`
-          ul_element.append(li_element)
-        })
-        document.querySelector("#emails-view").append(ul_element)
+        create_and_style_email(mails_from_inbox);
       }
     })
+}
 
-  } else if (mailbox === 'sent') {
+function load_sentbox(mailbox) {
 
-    const ul_element = document.createElement('ul')
-
-    fetch('/emails/sent')
+  fetch('/emails/sent')
     .then(response => response.json())
     .then(sent_emails => {
       if (sent_emails.length === 0) {
@@ -68,21 +74,35 @@ function load_mailbox(mailbox) {
         document.querySelector("#emails-view").append(message_element)
 
       } else {
-        sent_emails.forEach((mail)=> {
-          const li_element = document.createElement('li')
-          li_element.innerHTML = `${mail.sender} ${mail.subject} ${mail.timestamp}`  
-          ul_element.append(li_element)
-        })
+        create_and_style_email(sent_emails);
       }
     })
-    
-
-    document.querySelector('#emails-view').append(ul_element)
-
-  } else if (mailbox === 'archive') {
-
-  }
   
+}
+
+function load_archivebox() {
+
+}
+
+function create_and_style_email(mails_list) {
+
+  const ul_element = document.createElement('ul')
+  mails_list.forEach((mail) => {
+    
+    const li_element = document.createElement('li')
+    li_element.innerHTML = `
+            <div class="inbox-mail" ${mail.read ? "style='background-color: gray; color: white'" : "style='background-color: white'"}>
+              <div> 
+                <span class="sender">${mail.sender}</span>
+                <span class="subject">${mail.subject}</span>
+              </div>
+              <span class="timestamp" ${mail.read ? "style='color: white'" : ''}>${mail.timestamp}</span>
+            </div>
+            `
+    ul_element.append(li_element)
+  })
+  document.querySelector('#emails-view').append(ul_element)
+
 }
 
 function send_mail() {
@@ -90,21 +110,22 @@ function send_mail() {
   fetch('/emails', {
     method: 'POST',
     body: JSON.stringify({
-      recipients:`${document.querySelector('#compose-recipients').value}`,
+      recipients: `${document.querySelector('#compose-recipients').value}`,
       subject: `${document.querySelector('#compose-subject').value}`,
       body: `${document.querySelector('#compose-body').value}`
     })
   })
-  .then(response => response.json())
-  .then(result => {
+    .then(response => response.json())
+    .then(result => {
 
-    load_mailbox('sent');
-    console.log("hier is the result!");
-    console.log(result);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+      load_mailbox('sent');
+      console.log("hier is the result!");
+      console.log(result);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
 
   return false;
 }
+
